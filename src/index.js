08d3565e106d598343261c6265cf0d5dfad4490b
AustinBH <harlowaustin2@gmail.com>
2019-06-26 10:46:26 -0700

    met all deliverables

--- src/index.js
+++ src/index.js
@@ -1,6 +1,81 @@
 const fullname = document.getElementById("fullname");
+const email = document.getElementById("email")
+const street = document.getElementById("street")
+const city = document.getElementById("city")
+const state = document.getElementById("state")
+const postCode = document.getElementById("postcode")
+const phone = document.getElementById("phone")
+const cell = document.getElementById("cell")
+const dateOfBirth = document.getElementById("date_of_birth")
+const button = document.getElementsByTagName("button")[0];
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
+
+  button.addEventListener("click", () => {
+    addNewUser()
+  }
+)
 });
+
+function addNewUser(){
+  fetch("https://randomuser.me/api/")
+  .then(response => {
+    return response.json()
+  })
+  .then(json => {
+    // console.log(json)
+    addName(json.results[0].name)
+    addEmail(json.results[0].email)
+    addStreet(json.results[0].location)
+    addCity(json.results[0].location)
+    addState(json.results[0].location)
+    addPostCode(json.results[0].location)
+    addPhone(json.results[0].phone)
+    addCell(json.results[0].cell)
+    addDateOfBirth(json.results[0].dob)
+  })
+}
+
+function addName(object){
+  let name = `${object.title} ${object.first} ${object.last} `
+  fullname.textContent = name
+}
+
+function addEmail(string){
+  email.textContent = string
+}
+
+function addStreet(object) {
+  let newStreet = object.street
+  street.textContent = newStreet
+}
+
+function addCity(object) {
+  let newCity = object.city
+  city.textContent = newCity
+}
+
+function addState(object) {
+  let newState = object.state
+  state.textContent = newState
+}
+
+function addPostCode(object) {
+  let newPostCode = object.postcode
+  postCode.textContent = newPostCode
+}
+
+function addPhone(string) {
+  phone.textContent = string
+}
+
+function addCell(string) {
+  cell.textContent = string
+}
+
+function addDateOfBirth(object) {
+  let newDateOfBirth = object.date
+  dateOfBirth.textContent = newDateOfBirth
+}
